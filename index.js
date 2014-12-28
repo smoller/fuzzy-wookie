da4a6487256c9c4b8c1c182f81526e38b3be7c96
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-12-28 14:34:05 +0800

    One connection for redis only, added mean-seo for robot crawling and robots.txt

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-var RSVP      = require('rsvp');
-var express   = require('express')
-var logger    = require('morgan');
+var RSVP      = require('rsvp'),
+    express   = require('express'),
+    logger    = require('morgan'),
+    seo       = require('mean-seo'),
+    redis     = require('./lib/redis'),
+    logFormat = process.env.LOG_FORMAT || 'short',
+    appName   = process.env.APP_NAME || 'default';
+
 var app       = express();
-var logFormat = process.env.LOG_FORMAT || 'short';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(logger(logFormat));
 
-app.get('/*', function(request, response) {
-  var redis   = require('./lib/redis')();
-  var key     = request.query['key'];
-  var appName = process.env.APP_NAME || 'default';
-
-  redis.connect()
-  .then(function() {
-    if (!key) {
-      return redis.get(appName + ':current');
-    }
-
-    return RSVP.resolve(key);
-  })
-  .then(function(key) {
-    return redis.get(appName + ':' + key);
-  })
-  .then(function(data) {
-    if (data) {
-      response.send(data);
+//Handle robots
+app.use(function (req, res, next) {
+    if ('/robots.txt' == req.url) {
+        res.type('text/plain')
+        res.send("User-agent: *\nDisallow: /");
     } else {
-      response.status(404).send('Not Found');
+        next();
     }
-  })
-  .catch(function(error) {
-    console.log('Error retrieving data from Redis: ', reason);
-  })
-  .finally(function() {
+});
+
+app.use(seo({
+    cacheClient: 'redis', // Can be 'disk' or 'redis'
+    cacheDuration: 2 * 60 * 60 * 24 * 1000, // In milliseconds
+    redisUrl: redis.redisUrl
+}));
+
+app.get('/*', function(request, response) {
+    var key = request.query['key'];
+
+    redis.connect().then(function() {
+        if (!key) {
+            return redis.get(appName + ':current');
+        }
+
+        return RSVP.resolve(key);
+    })
+    .then(function(key) {
+        return redis.get(appName + ':' + key);
+    })
+    .then(function(data) {
+        if (data) {
+            response.send(data);
+        } else {
+            response.status(404).send('Not Found');
+        }
+    })
+    .catch(function(error) {
+        console.log('Error retrieving data from Redis: ', reason);
+    });
+});
+
+app.on('close', function() {
     redis.quit();
-  });
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 });
